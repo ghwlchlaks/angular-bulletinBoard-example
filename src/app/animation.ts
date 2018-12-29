d287e8c330e753b61e3c151354a63542b693d715
jiho <ghwlchlaks@naver.com>
2018-12-29 21:43:37 +0900

    home1 board animation homePage <=> BoardPage

--- src/app/animation.ts
+++ src/app/animation.ts
@@ -3,20 +3,8 @@ import {
     transition, animate, style, query, state
   } from '@angular/animations';
 // Routable animations
-export const slideInAnimation1 =
-trigger('routeAnimation', [
-  state('*', style({
-    backgroundColor: 'rgba(0,0,0,0)'
-  })),
-  transition('main => serve', [style({top: '100%'}), animate('.3s', style({
-    top: 0
-  }))]),
 
-  transition('serve => main', [style({bottom: '100%'}), animate('.3s', style({
-    bottom: 0
-  }))])
-]);
-
+/* 하나의 route-oultlet에 한개의 trigger */
 export const slideInAnimation2 =
 trigger('routeAnimation', [
   state('*', style({
@@ -28,6 +16,15 @@ trigger('routeAnimation', [
   }))]),
 
   transition('serve => main', [style({bottom: '100%'}), animate('.3s', style({
+    bottom: 0
+  }))]),
+
+
+  transition('HomePage => BoardPage', [style({top: '100%'}), animate('.3s', style({
+    top: 0
+  }))]),
+
+  transition('BoardPage => HomePage', [style({bottom: '100%'}), animate('.3s', style({
     bottom: 0
   }))])
 ]);
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routers: Routes = [
-  {path: '', component: HomeComponent, data: {animations: 'HomePage'}},
-  {path: 'board', loadChildren: './contents/contents.module#ContentsModule', data: {animations: 'BoardPage'}},
+  {path: '', component: HomeComponent, data: {animation: 'HomePage'}},
+  {path: 'board', loadChildren: './contents/contents.module#ContentsModule', data: {animation: 'BoardPage'}},
   {path: 'board1', loadChildren: './contents1/contents1.module#Contents1Module'}
 ];
 
