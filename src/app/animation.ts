86f616bbe0fb73ec5c42b597d83651aab825406a
jiho <ghwlchlaks@naver.com>
2018-12-29 21:29:47 +0900

    contents1 anmation main <=> serve

--- src/app/animation.ts
+++ src/app/animation.ts
@@ -1,88 +1,34 @@
 import {
     trigger, animateChild, group,
-    transition, animate, style, query
+    transition, animate, style, query, state
   } from '@angular/animations';
 // Routable animations
-export const slideInAnimation =
-trigger('routeAnimation', [
-    transition('HomePage <=> BoardPage', [
-    style({ position: 'relative' }),
-    query(':enter, :leave', [
-        style({
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%'
-        })
-    ]),
-    query(':enter', [
-        style({ left: '-100%'})
-    ]),
-    query(':leave', animateChild()),
-    group([
-        query(':leave', [
-        animate('300ms ease-out', style({ left: '100%'}))
-        ]),
-        query(':enter', [
-        animate('300ms ease-out', style({ left: '0%'}))
-        ])
-    ]),
-    query(':enter', animateChild()),
-    ])
-]);
-
 export const slideInAnimation1 =
 trigger('routeAnimation', [
-  transition('BoardPage <=> AddPage', [
-    style({ position: 'relative' }),
-    query(':enter, :leave', [
-      style({
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%'
-      })
-    ]),
-    query(':enter', [
-      style({ left: '-100%'})
-    ]),
-    query(':leave', animateChild()),
-    group([
-      query(':leave', [
-        animate('300ms ease-out', style({ left: '100%'}))
-      ]),
-      query(':enter', [
-        animate('300ms ease-out', style({ left: '0%'}))
-      ])
-    ]),
-    query(':enter', animateChild()),
-  ])
+  state('*', style({
+    backgroundColor: 'rgba(0,0,0,0)'
+  })),
+  transition('main => serve', [style({top: '100%'}), animate('.3s', style({
+    top: 0
+  }))]),
+
+  transition('serve => main', [style({bottom: '100%'}), animate('.3s', style({
+    bottom: 0
+  }))])
 ]);
 
 export const slideInAnimation2 =
 trigger('routeAnimation', [
-  transition('main <=> serve', [
-    style({ position: 'relative' }),
-    query(':enter, :leave', [
-      style({
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%'
-      })
-    ]),
-    query(':enter', [
-      style({ left: '-100%'})
-    ]),
-    query(':leave', animateChild()),
-    group([
-      query(':leave', [
-        animate('300ms ease-out', style({ left: '100%'}))
-      ]),
-      query(':enter', [
-        animate('300ms ease-out', style({ left: '0%'}))
-      ])
-    ]),
-    query(':enter', animateChild()),
-  ])
+  state('*', style({
+    position: 'absolute',
+    backgroundColor: 'rgba(0,0,0,0)'
+  })),
+  transition('main => serve', [style({top: '100%'}), animate('.3s', style({
+    top: 0
+  }))]),
+
+  transition('serve => main', [style({bottom: '100%'}), animate('.3s', style({
+    bottom: 0
+  }))])
 ]);
+
